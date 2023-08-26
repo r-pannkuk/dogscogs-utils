930e1cbe544ce8da3c0ffb00fca10aced6a51f7c
Riley Pannkuk <Riley.Pannkuk@doubledown.com>
2023-08-26 05:07:09 -0700

    Color deconstruction.

--- dogscogs_utils/cogs/reactcog.py
+++ dogscogs_utils/cogs/reactcog.py
@@ -507,7 +507,7 @@ class ReactCog(DogCog):
         embed.description = replace_tokens(
             random.choice(await self._responses(guild=guild)()), member
         )
-        embed.colour = discord.Color.from_rgb(*embed.colour)
+        embed.colour = discord.Color.from_rgb(*(embed.colour or (0, 0, 0)))
 
         if "footer" in embed_config and embed_config["footer"] != "":
             embed.set_footer(
