af9213efa4641e6b2c42f0bef54af6ed704270a3
Riley Pannkuk <Klypyon@gmail.com>
2024-09-11 10:49:27 -0700

    Forgot the name...

--- dogscogs/typings/dogscogs/converters/date.pyi
+++ dogscogs/typings/dogscogs/converters/date.pyi
@@ -1,4 +1,3 @@
-import abc
 import datetime
 from ..constants import TIMEZONE as TIMEZONE
 from ..core.converter import DogCogConverter as DogCogConverter
@@ -8,8 +7,8 @@ from redbot.core import commands as commands
 
 def date_formats() -> Generator[Incomplete, None, None]: ...
 
-class BirthdayConverter(DogCogConverter, metaclass=abc.ABCMeta):
+class BirthdayConverter(DogCogConverter):
     """Returns a datetime object ignoring the birthday for a birthday string.
     """
     @staticmethod
-    async def convert(_ctx: commands.Context, argument: str) -> datetime.datetime: ...
+    async def parse(_ctx: commands.Context, argument: str) -> datetime.datetime: ...
