6bcdb706b32af3bc05e5b3408d52f14b9b6cfeef
Riley Pannkuk <Klypyon@gmail.com>
2024-09-14 08:33:21 -0700

    Number Prompt TextInput (as opposed to just a modal).

--- dogscogs/views/prompts.py
+++ dogscogs/views/prompts.py
@@ -3,6 +3,33 @@ import discord
 
 from ..converters.percent import Percent
 
+class NumberPromptTextInput(discord.ui.TextInput):
+    def __init__(
+        self,
+        *args,
+        min : typing.Optional[int] = None,
+        max : typing.Optional[int] = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.min = min
+        self.max = max
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        try:
+            value = await Percent.parse(None, self.value) # type: ignore[arg-type]
+        except:
+            raise ValueError("Please enter a valid number.")
+
+        if self.min is not None and value < self.min:
+            raise ValueError(f"Please enter a number greater than {self.min}.")
+
+        if self.max is not None and value > self.max:
+            raise ValueError(f"Please enter a number less than {self.max}.")
+
+        return True
+        
+
 class NumberPromptModal(discord.ui.Modal):
     def __init__(
         self,
@@ -21,34 +48,34 @@ class NumberPromptModal(discord.ui.Modal):
             title=title,
         )
 
-        self.item: discord.ui.TextInput = discord.ui.TextInput(
+        self.item: NumberPromptTextInput = NumberPromptTextInput(
             label=label,
             placeholder=placeholder,
             required=True,
             style=discord.TextStyle.short,
             custom_id=custom_id,
             row=row,
+            min=min,
+            max=max,
         )
 
         self.add_item(self.item)
 
-        self.min: int = min
-        self.max: int = max
         self.author: typing.Union[discord.Member, discord.User] = author
 
     async def interaction_check(self, interaction: discord.Interaction) -> bool:
         if interaction.user != self.author:
             return False
 
-        try:
-            value = await Percent.parse(None, self.item.value) # type: ignore[arg-type]
-        except:
-            raise ValueError("Please enter a valid number.")
+        # try:
+        #     value = await Percent.parse(None, self.item.value) # type: ignore[arg-type]
+        # except:
+        #     raise ValueError("Please enter a valid number.")
 
-        if value < self.min or value > self.max:
-            raise ValueError(
-                f"Please enter a number between {self.min} and {self.max}."
-            )
+        # if value < self.min or value > self.max:
+        #     raise ValueError(
+        #         f"Please enter a number between {self.min} and {self.max}."
+        #     )
 
         return True
 
